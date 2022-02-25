c99fd9702f2e972fcd01eb63e5c086740ec2b295
GleisonOliveira <gleison@hotmail.com.br>
2022-02-25 01:52:46 -0300

    add node sass process

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,30 @@ import mustache from 'mustache-express';
 import dotenv from 'dotenv';
 import path from 'path';
 import routes from './routes';
+import sass from 'node-sass';
+import fs from 'fs';
+
+const sassPathDest = path.join(__dirname, '../dist/css/main.css');
+const sassPath = path.join(__dirname, 'sass/main.scss');
+
+sass.render(
+  {
+    file: sassPath,
+    outputStyle: 'compressed',
+  },
+  function (error, result) {
+    if (!error) {
+      fs.writeFile(sassPathDest, result.css, function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      return;
+    }
+
+    console.log(error);
+  }
+);
 
 dotenv.config();
 
@@ -14,6 +38,7 @@ server.engine('mustache', mustache());
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, '../public')));
+server.use(express.static(path.join(__dirname, '../dist')));
 server.use(routes);
 server.use((req: Request, res: Response) => {
   res.status(404).send('Não encontrado');
