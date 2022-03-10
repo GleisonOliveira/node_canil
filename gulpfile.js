f100ae8553df65132a451f3726a827d3e6794c20
GleisonOliveira <gleison@hotmail.com.br>
2022-03-09 23:39:45 -0300

    add gulp uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
 const { src, dest, task } = require('gulp');
+var uglify = require('gulp-uglify');
+var pipeline = require('readable-stream').pipeline;
 
 async function copy() {
-  src('node_modules/bootstrap/dist/*/*').pipe(dest('dist/'));
-  src('node_modules/@fortawesome/fontawesome-free/css/*.*').pipe(
-    dest('dist/css')
-  );
-  src('node_modules/@fortawesome/fontawesome-free/svgs/*.*').pipe(
-    dest('dist/svgs')
-  );
-  src('node_modules/@fortawesome/fontawesome-free/webfonts/*.*').pipe(
-    dest('dist/webfonts')
-  );
+	src('node_modules/bootstrap/dist/*/*').pipe(dest('dist/'));
+	src('node_modules/@fortawesome/fontawesome-free/css/*.*').pipe(
+		dest('dist/css')
+	);
+	src('node_modules/@fortawesome/fontawesome-free/svgs/*.*').pipe(
+		dest('dist/svgs')
+	);
+	src('node_modules/@fortawesome/fontawesome-free/webfonts/*.*').pipe(
+		dest('dist/webfonts')
+	);
 }
 
-task(copy);
+task('uglify', function() {
+	return src('dist/js/scripts/*.js')
+    .pipe(uglify())
+    .pipe(dest('dist/js/scripts'))
+    .on('error', function(err) {
+      console.error('Error in compress task', err.toString());
+    });
+});
+
+exports.copy = copy;
